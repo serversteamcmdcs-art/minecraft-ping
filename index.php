01ff97bc59ca0010e70cb4ee6c43dd5b78842cec
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-04-09 22:38:36 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php imclude('MinecraftMine.php'); ?>
+<!DOCTYPE html>
+<html lang="en"
+<body>
+  <div class="MyServerBlock">
+      <div class="OnlineServer"><?php echo $online?> / <?php echo $onlineMax;?></div>
+  </div>
+</body>
+</html>
