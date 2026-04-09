0cdea8b88d406f6f4d3edc96d4120602e53fc489
serversteamcmdcs-art <serversteamcmdcs@gmail.com>
2026-04-09 21:39:58 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+	require __DIR__ . '/src/MinecraftPing.php';
+	require __DIR__ . '/src/MinecraftPingException.php';
+	
+	use xPaw\MinecraftPing;
+	use xPaw\MinecraftPingException;
+	
+	try
+	{
+		$Query = new MinecraftPing( 'free.minecraft-hoster.com', 35466 );
+		
+		print_r( $Query->Query() );
+	}
+	catch( MinecraftPingException $e )
+	{
+		echo $e->getMessage();
+	}
+	finally
+	{
+		if( $Query )
+		{
+			$Query->Close();
+		}
+	}
+?>
